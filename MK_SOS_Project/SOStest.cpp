7363b5c807498c1b3f50d73118dd7c81557c2d3f
Maktcr2022 <masonrob01@live.com>
2024-10-10 16:26:21 -0500

    unit testing
    starting to implement unit testing

---
+++ MK_SOS_Project/SOStest.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include <QTest>
+
+
+class SOStest : public QObject {
+
+    Q_OBJECT
+
+private slots:
+    void createGrid();
+    void clearGrid();
+};
+
+void SOStest::createGrid() {
+
+}
